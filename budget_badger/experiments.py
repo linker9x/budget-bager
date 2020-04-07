2588e01f47754b0f858a106f2695a09eed4e2676
Unknown <lisa.winkler@stud.uni-hannover.de>
2020-04-07 15:36:33 -0400

    Slow and steady.

--- budget_badger/experiments.py
+++ budget_badger/experiments.py
@@ -5,10 +5,12 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 sys.path.append(os.path.abspath(os.path.join('.')))
 
 import pandas as pd
-#import budget_badger.classifier.expense_classifier as ec
+import budget_badger.classifier.expense_classifier as ec
 
 classified_exp = '../data/classified_exp.csv'
 
 if __name__ == "__main__":
     exp_df = pd.read_csv(classified_exp)
-    print(exp_df.head())
+    print("Expense Data Shape: {}".format(exp_df.shape))
+    print("Expense Value Counts: \n{}".format(exp_df['Category'].value_counts()))
+
